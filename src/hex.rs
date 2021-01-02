4310edbedd6784b1d35724f7e15db8dee1a7f00d
Saurabh Rawat <saurabh.rawat90@gmail.com>
2021-01-02 14:36:11 +0530

    set1

--- src/hex.rs
+++ src/hex.rs
@@ -13,6 +13,7 @@ pub const HEX: [char; 16] = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f',
 ];
 
+#[derive(Debug)]
 pub struct Hex(pub String);
 
 impl Hex {
